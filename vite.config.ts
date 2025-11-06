e42649669027124e7850691fa9422229f2a0355b
xaviergoh <38094356+xaviergoh@users.noreply.github.com>
2025-11-06 20:55:05 +0800

    Update vite.config.ts
    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/fx-treasury/', // Replace with your GitHub repo name
+  base: '/treasury-mgmt-poc/', // Replace with your GitHub repo name
   server: {
     host: "::",
     port: 8080,

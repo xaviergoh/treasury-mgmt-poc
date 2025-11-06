57739639fe7758d8a1582a0371ffb43278e01fe3
xaviergoh <38094356+xaviergoh@users.noreply.github.com>
2025-11-06 21:04:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/treasury-mgmt-poc/' : '/',
+  base: '/treasury-mgmt-poc/',
   server: {
     host: "::",
     port: 8080,
